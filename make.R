7efa70d90c46f34d0da2c79ea6c15517f94b86ee
Sang B. Mendy <sangbm7@gmail.com>
2025-04-04 16:20:22 +0100

    init repo

---
+++ make.R
@@ -0,0 +1,31 @@
+#' Rcomp: A Research Compendium
+#' 
+#' @description 
+#' A paragraph providing a full description of the project and describing each 
+#' step of the workflow.
+#' 
+#' @author Sang B. Mendy \email{sangbm7@@gmail.com}
+#' 
+#' @date 2025/04/04
+
+
+
+## Install Dependencies (listed in DESCRIPTION) ----
+
+devtools::install_deps(upgrade = "never")
+
+
+## Load Project Addins (R Functions and Packages) ----
+
+devtools::load_all(here::here())
+
+
+## Global Variables ----
+
+# You can list global variables here (or in a separate R script)
+
+
+## Run Project ----
+
+# List all R scripts in a sequential order and using the following form:
+# source(here::here("analyses", "script_X.R"))
